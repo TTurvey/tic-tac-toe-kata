c56f549eec34062f0f9490522f1957df9c6f51ac
CrisMouta <crismouta@gmail.com>
2022-09-07 11:08:37 +0200

    second game state

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,6 +1,12 @@
 public class TicTacToe {
     public String[] play(String x, int i) {
-        String[] gridArray = {"X", ".", ".", ".", ".", ".", ".", ".", "."};
+        String[] gridArray = {".", ".", ".", ".", ".", ".", ".", ".", "."};
+        if (i == 0) {
+            gridArray[0] = "X";
+        }
+        if (i == 1) {
+            gridArray[1] = "O";
+        }
         return gridArray;
     }
 }
--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -8,9 +8,17 @@ class TicTacToeTest {
 
     @Test
     public void
-    should_return_state_of_the_grid() {
+    should_return_position_zero() {
         String[] expectedOutput = {"X", ".", ".", ".", ".", ".", ".", ".", "."};
         String[] output = new TicTacToe().play("X", 0 );
+        assertTrue( Arrays.equals(expectedOutput, output ));
+    }
+
+    @Test
+    public void
+    should_return_position_one() {
+        String[] expectedOutput = {".", "O", ".", ".", ".", ".", ".", ".", "."};
+        String[] output = new TicTacToe().play("O", 1 );
         assertTrue( Arrays.equals(expectedOutput, output ));
     }
 
