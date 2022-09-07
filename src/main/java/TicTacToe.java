5f76a0fd1e381f193568084e9cf1e433f9ee66bf
Tim Turvey <tturvey@hotmail.co.uk>
2022-09-07 11:46:59 +0100

    Green: checks for horizontal wins

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -16,13 +16,16 @@ public class TicTacToe {
 
     public String checkGame() {
         int[][] winingPositions = {{0,1,2}, {3,4,5}, {6,7,8}};
-        System.out.println(Arrays.toString(winingPositions));
-        /*for (int i = 0; i < winingPositions.length; i++) {
-            if (winingPositions[i][0] == "0") {
+
+        for (int i = 0; i < winingPositions.length; i++) {
+            if ( gridArray[winingPositions[i][0]].equals("X") && gridArray[winingPositions[i][1]].equals("X") && gridArray[winingPositions[i][2]].equals("X") ) {
+                return "Player X wins";
+            }
+            if ( gridArray[winingPositions[i][0]].equals("0") && gridArray[winingPositions[i][1]].equals("0") && gridArray[winingPositions[i][2]].equals("0") ) {
                 return "Player 0 wins";
             }
         }
-*/
+
         return "Game still being played";
 
     }
