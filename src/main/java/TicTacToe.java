32745616ce66e5261c2dc629e7142d9d2bbdc2af
Tim Turvey <tturvey@hotmail.co.uk>
2022-09-07 09:56:16 +0100

    Green: passes first move test

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,2 +1,8 @@
 public class TicTacToe {
+    public String[] play(String x, int i) {
+        String[] gridArray = {"X", ".", ".", ".", ".", ".", ".", ".", "."};
+        return gridArray;
+    }
 }
+
+
--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -1,5 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TicTacToeTest {
+
+    @Test
+    public void
+    should_return_state_of_the_grid() {
+        String[] expectedOutput = {"X", ".", ".", ".", ".", ".", ".", ".", "."};
+        String[] output = new TicTacToe().play("X", 0 );
+        assertTrue( Arrays.equals(expectedOutput, output ));
+    }
 
 }
