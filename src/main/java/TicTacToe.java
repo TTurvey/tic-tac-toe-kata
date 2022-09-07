d341abe28eb92a6bc9cfcf194c6a7af965a0dfda
CrisMouta <crismouta@gmail.com>
2022-09-07 12:39:36 +0200

    Red::checking game win positions

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -13,6 +13,19 @@ public class TicTacToe {
         System.out.println(Arrays.toString(gridArray));
         return gridArray;
     }
+
+    public String checkGame() {
+        int[][] winingPositions = {{0,1,2}, {3,4,5}, {6,7,8}};
+        System.out.println(Arrays.toString(winingPositions));
+        /*for (int i = 0; i < winingPositions.length; i++) {
+            if (winingPositions[i][0] == "0") {
+                return "Player 0 wins";
+            }
+        }
+*/
+        return "Game still being played";
+
+    }
 }
 
 
--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -50,7 +50,7 @@ class TicTacToeTest {
 
     @Test
     public void
-    should_return_error_if_0_plays_first() {
+    should_return_moves() {
         ticTacToe.play("X", 0 );
         String[] output = ticTacToe.play("X", 1 );
 
@@ -58,5 +58,57 @@ class TicTacToeTest {
 
         assertArrayEquals(expectedOutput, output);
     }
+
+    @Test
+    public void
+    should_return_player_X_wins_first_row() {
+        ticTacToe.play("X", 0 );
+        ticTacToe.play("X", 1 );
+        String[] output = ticTacToe.play("X", 2 );
+
+        String[] expectedOutput = {"X", "X", "X", ".", ".", ".", ".", ".", "."};
+        String message = "Player X wins";
+
+        assertEquals(message, ticTacToe.checkGame());
+    }
+
+    @Test
+    public void
+    should_return_player_0_wins_first_row() {
+        ticTacToe.play("0", 0 );
+        ticTacToe.play("0", 1 );
+        String[] output = ticTacToe.play("0", 2 );
+
+        String[] expectedOutput = {"0", "0", "0", ".", ".", ".", ".", ".", "."};
+        String message = "Player 0 wins";
+
+        assertEquals(message, ticTacToe.checkGame());
+    }
+
+    @Test
+    public void
+    should_return_player_X_wins_second_row() {
+        ticTacToe.play("X", 3 );
+        ticTacToe.play("X", 4 );
+        String[] output = ticTacToe.play("X", 5 );
+
+        String[] expectedOutput = {".", ".", ".", "X", "X", "X", ".", ".", "."};
+        String message = "Player X wins";
+
+        assertEquals(message, ticTacToe.checkGame());
+    }
+
+   /* @Test
+    public void
+    should_return_player_0_wins_second_row() {
+        ticTacToe.play("0", 3 );
+        ticTacToe.play("0", 4 );
+        String[] output = ticTacToe.play("0", 5 );
+
+        String[] expectedOutput = {"0", "0", "0", ".", ".", ".", ".", ".", "."};
+        String message = "Player 0 wins";
+
+        assertEquals(message, ticTacToe.checkGame());
+    }*/
 
 }
